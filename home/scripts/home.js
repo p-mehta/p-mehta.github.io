5807e1636fd1d781444271649cf5d69a76116c03
Pratik Mehta <mehta.pratik1985@gmail.com>
2015-12-17 14:40:16 -0800

    Added more content
    Completed A&T and Contact section

--- home/scripts/home.js
+++ home/scripts/home.js
@@ -7,6 +7,20 @@
 
 var myApp = angular.module('myApp', ['ui.bootstrap']);
 
+myApp.directive('linkSocial', [function() {
+	return {
+		restrict: 'AE',
+		replace: true,
+		templateUrl: 'fragments/social.html',
+		scope: {
+			type: '=type'
+		},
+		link: function($scope, element, attrs) {
+			
+		}
+	};
+}]);
+
 myApp.controller('mainController', ['$scope', '$location', '$timeout', 
                                     function($scope, $location, $timeout) {
 	$scope.message = 'Dev in progress...';
