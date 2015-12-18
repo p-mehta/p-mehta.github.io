9e508a3c79690b1b664dd2b6bb13762716e871d8
Pratik Mehta <mehta.pratik1985@gmail.com>
2015-12-18 03:41:47 -0800

    Set active tab when scroll to corresponding section

--- home/scripts/home.js
+++ home/scripts/home.js
@@ -48,20 +48,67 @@ myApp.directive('slideShow', [function() {
 	};
 }]);
 
-myApp.controller('mainController', ['$scope', '$location', '$timeout', '$filter',
-                                    function($scope, $location, $timeout, $filter) {
+myApp.controller('mainController', ['$scope', '$location', '$timeout', '$filter', '$window',
+                                    function($scope, $location, $timeout, $filter, $window) {
 	$scope.message = 'Dev in progress...';
 	
-	$scope.activeTab = 'home';
+	$scope.activeTab = {
+			home: true,
+			resume: false,
+			others: false,
+			contact: false
+	};
 	
-	$scope.setActive = function(tab) {
-		$scope.activeTab = tab;
-		
-		$timeout(function() {
-			$("html, body").animate({scrollTop: $('section.active').offset().top }, "slow");
+	var setActiveTab = function(tab) {
+		angular.forEach($scope.activeTab, function(value, key) {
+			if (key == tab) {
+				$scope.activeTab[key] = true;
+			} else {
+				$scope.activeTab[key] = false;
+			}
 		});
 	};
 	
+	var onScroll = function() {
+		var scrollPos = angular.element($window).scrollTop() + $window.innerHeight/2;
+	    $('section').each(function () {
+	        var currLink = $(this);
+	        var contactElem = $('section.contactSection');
+	        if (contactElem.position().top < scrollPos + contactElem.height() ) { 
+        		$('section').removeClass('active');
+        		contactElem.addClass('active');
+        		setActiveTab('contact');
+    		} else if (currLink.position().top <= scrollPos && currLink.position().top + currLink.height() > scrollPos) {
+	            $('section').removeClass('active');
+	            currLink.addClass('active');
+	    		if (currLink.hasClass('othersSection')) {
+	    			setActiveTab('others');
+	    		}
+	    		if (currLink.hasClass('resumeSection')) {
+	    			setActiveTab('resume');
+	    		}
+	    		if (currLink.hasClass('homeSection')) {
+	    			setActiveTab('home');
+	    		}
+	        } else {
+	            currLink.removeClass('active');
+	        }
+	        $scope.$apply();
+	    });
+    };
+	
+	$scope.setActive = function(tab) {
+		if ($scope.activeTab == tab) { return; }
+		angular.element($window).off('scroll');
+		setActiveTab(tab);
+		$timeout(function() {
+			$('html, body').animate({scrollTop: $('section.active').offset().top }, 'slow', 'swing', function() {
+				window.location.hash = $('section.active');
+				angular.element($window).on('scroll', onScroll);
+			});
+		});
+	};
+    
 	$scope.albums = [
 	                 { name: 'SF', images: [
 	                                        { image: 'images/slides/SF.jpg'}
@@ -94,6 +141,7 @@ myApp.controller('mainController', ['$scope', '$location', '$timeout', '$filter'
 	
 	var init = function() {
 		$scope.setActive('home');
+		angular.element($window).on('scroll', onScroll);
 	};
 	
 	init();
